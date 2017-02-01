169c338231c18137a583cfd9b13053280fa796b2
Enether <familyguyuser192@windowslive.com>
2017-02-01 12:38:06 +0200

    Test for home_page() and logic in it

--- testing_goat/superlists/lists/tests.py
+++ testing_goat/superlists/lists/tests.py
@@ -1,5 +1,6 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
 from lists.views import home_page
 
 
@@ -8,3 +9,11 @@ class HomePageTests(TestCase):
         result = resolve('/')
         # Should be tied to the same function
         self.assertEqual(result.func, home_page)
+
+    def test_response_page_returns_correct_html(self):
+        request = HttpRequest
+        response = home_page(request)
+
+        self.assertTrue(response.content.startswith(b"<html>"))
+        self.assertContains(response, "<title>To-Do list</title>")
+        self.assertTrue(response.content.endswith(b"<html>"))
