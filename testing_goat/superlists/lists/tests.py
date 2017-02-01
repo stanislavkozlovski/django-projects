e10355a84ee00796828c832d46543a74a3bc072c
Enether <familyguyuser192@windowslive.com>
2017-02-01 12:29:44 +0200

    Updated .gitignore, added a test and a home_page func

--- testing_goat/superlists/lists/tests.py
+++ testing_goat/superlists/lists/tests.py
@@ -1,3 +1,10 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from lists.views import home_page
 
-# Create your tests here.
+
+class HomePageTests(TestCase):
+    def test_root_url_uses_home_page_view(self):
+        result = resolve('/')
+        # Should be tied to the same function
+        self.assertEqual(result.func, home_page)
