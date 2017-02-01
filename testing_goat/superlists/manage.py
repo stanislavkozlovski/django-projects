ed66a6e72294a71487a1b77c04194f1e0bc66d9d
Enether <familyguyuser192@windowslive.com>
2017-02-01 11:13:06 +0200

    Overrode the default port for the app

--- testing_goat/superlists/manage.py
+++ testing_goat/superlists/manage.py
@@ -5,6 +5,10 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "superlists.settings")
     try:
+        # Override default port for `runserver` command
+        from django.core.management.commands.runserver import Command as runserver
+
+        runserver.default_port = "1337"
         from django.core.management import execute_from_command_line
     except ImportError:
         # The above import may fail for some other reason. Ensure that the
