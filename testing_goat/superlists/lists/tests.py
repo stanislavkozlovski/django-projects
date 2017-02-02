a34c8be6d2d12d149954190c9f4690182717c982
Enether <familyguyuser192@windowslive.com>
2017-02-02 11:05:22 +0200

    Added an Item model, tests for it and two migrations

--- testing_goat/superlists/lists/tests.py
+++ testing_goat/superlists/lists/tests.py
@@ -5,6 +5,7 @@ from django.template.loader import render_to_string
 from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
+from lists.models import Item
 
 
 class HomePageTests(TestCase):
@@ -42,3 +43,24 @@ class HomePageTests(TestCase):
             self.remove_csrf(html_code1),
             self.remove_csrf(html_code2)
         )
+
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        f_item_text = 'First item ever!'
+        first_item = Item()
+        first_item.text = f_item_text
+        first_item.save()
+
+        s_item_text = 'Second!'
+        second_item = Item()
+        second_item.text = s_item_text
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, f_item_text)
+        self.assertEqual(second_saved_item.text, s_item_text)
