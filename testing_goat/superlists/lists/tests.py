9ba5c0f527c5ed3510fb4e2b858cd470f442da5d
Enether <familyguyuser192@windowslive.com>
2017-02-02 10:41:04 +0200

    Added a unit-test for POST request and fixed the old test to factor in CSRF

--- testing_goat/superlists/lists/tests.py
+++ testing_goat/superlists/lists/tests.py
@@ -1,3 +1,5 @@
+import re
+
 from django.core.urlresolvers import resolve
 from django.template.loader import render_to_string
 from django.test import TestCase
@@ -12,8 +14,28 @@ class HomePageTests(TestCase):
         self.assertEqual(result.func, home_page)
 
     def test_response_page_returns_correct_html(self):
-        request = HttpRequest
+        request = HttpRequest()
         response = home_page(request)
 
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        expected_html = render_to_string('home.html', request=request)
+        self.assertEqualExceptCSFR(response.content.decode(), expected_html)
+
+    def test_post_request_home_page(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'Coffee'
+
+        response = home_page(request)
+
+        self.assertIn('Coffee', response.content.decode())
+
+    @staticmethod
+    def remove_csrf(html_code):
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        return re.sub(csrf_regex, '', html_code)
+
+    def assertEqualExceptCSFR(self, html_code1, html_code2):
+        return self.assertEqual(
+            self.remove_csrf(html_code1),
+            self.remove_csrf(html_code2)
+        )
