e38b9cf48ef71b48162382c8eb7137bc9aede4c1
Enether <familyguyuser192@windowslive.com>
2017-02-05 16:34:41 +0200

    Functional tests for page styling + some comments in views.py

--- testing_goat/superlists_/functional_tests/tests.py
+++ testing_goat/superlists_/functional_tests/tests.py
@@ -72,3 +72,23 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertRowNotInTable('2: Buy Coffee')
         self.assertRowInTable('1: Test things')
 
+    def test_layout_and_styling(self):
+        # The Aesthetic Goat visits our page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+        # It notices that the input box is nicely centered
+        input_box = self.browser.find_element_by_id('new_item')
+        self.assertAlmostEqual(
+            input_box.location['x'] + input_box.size['width'] / 2,
+            512,
+            delta=6
+        )
+
+        # It starts a new list and sees that the input box is nicely centered there too
+        input_box.send_keys('IsItCentered?\n')
+        other_input_box = self.browser.find_element_by_id('new_item')
+        self.assertAlmostEqual(
+            other_input_box.location['x'] + input_box.size['width'] / 2,
+            512,
+            delta=6
+        )
--- testing_goat/superlists_/lists/views.py
+++ testing_goat/superlists_/lists/views.py
@@ -3,16 +3,18 @@ from django.http import HttpRequest, HttpResponse
 from lists.models import Item, List
 
 
+# /
 def home_page(request: HttpRequest):
     return render(request, 'home.html')
 
 
+# @lists/{list_id}
 def view_list(request: HttpRequest, list_id: str):
     list_: List = List.objects.get(id=list_id)
-    items = Item.objects.filter(list=list_)
     return render(request, 'list.html', {'list': list_})
 
 
+# @lists/new
 def new_list(request: HttpRequest):
     """ Creates a new TODO list with the new item """
     new_item_text = request.POST['item_text']
@@ -22,6 +24,7 @@ def new_list(request: HttpRequest):
     return redirect(f'/lists/{new_list_.id}')
 
 
+# @lists/{list_id}/add_item
 def add_item(request: HttpRequest, list_id: str):
     """ Creates and adds a new item to an existing TODO list """
     new_item_text = request.POST['item_text']
