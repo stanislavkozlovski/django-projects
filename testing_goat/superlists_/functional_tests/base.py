22b5c92b4dfe44f874881ffe6b643ee21d4e1f3f
Enether <familyguyuser192@windowslive.com>
2017-02-13 15:20:53 +0200

    Refactored FT and added one for authentication

--- testing_goat/superlists_/functional_tests/base.py
+++ testing_goat/superlists_/functional_tests/base.py
@@ -1,6 +1,9 @@
 import sys
+import time
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+MAX_WAIT = 10
 
 
 class FunctionalTest(StaticLiveServerTestCase):
@@ -33,6 +36,18 @@ class FunctionalTest(StaticLiveServerTestCase):
         table = self.browser.find_element_by_id('list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertNotIn(desired_row, [row.text for row in rows])
+
+    def wait_for(self, fn):
+        start_time = time.time()
+        while True:
+            try:
+                return fn()
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    # Time is up and our test is failing!
+                    raise e
+
+                time.sleep(0.5)
 
     def get_item_input_box(self):
         return self.browser.find_element_by_id('id_text')
---
+++ testing_goat/superlists_/functional_tests/test_login.py
@@ -0,0 +1,47 @@
+import re
+
+from selenium.webdriver.common.keys import Keys
+from django.core import mail
+
+from functional_tests.base import FunctionalTest
+
+
+TEST_EMAIL = "Somebody@abv.bg"
+SUBJECT = "Your login link for Superlists"
+
+
+class LoginTest(FunctionalTest):
+
+    def test_can_get_email_link_to_log_in(self):
+        # The Log Goat visits our website and sees a form for logging in
+        # It prompts it for an email address so he adds his
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_name('email').send_keys(TEST_EMAIL)
+        self.browser.find_element_by_name('email').send_keys(Keys.ENTER)
+
+        # A message appears telling it the email has been sent
+        self.wait_for(lambda: self.assertIn(
+            "Check your email, you'll find a message with a link that will log you into the site.",
+            self.browser.find_element_by_tag_name('body').text
+        ))
+
+        # It opens it's email and finds a message
+        email = mail.outbox[0]
+        self.assertIn(TEST_EMAIL, email.to)
+        self.assertEqual(email.subject, SUBJECT)
+
+        # It has an url link to it
+        self.assertIn('Use this link to log in', email.body)
+        url_search = re.search(r'http://.+/.+$', email.body)
+        if not url_search:
+            self.fail("Could not find the login URL in the sent e-mail.")
+        url = url_search.group(0)
+        self.assertIn(self.live_server_url, url)  # the base URLs must be identical
+
+        # the goat clicks the URL
+        self.browser.get(url)
+
+        # it is logged in!
+        self.wait_for(lambda: self.browser.find_element_by_link_text('Sign out'))
+        nav_bar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn(TEST_EMAIL, nav_bar.text)
